7342401181d55fb89bcb6d7d03605f7ea1c9064a
Cooper <87213885+WorthAmoeba5637@users.noreply.github.com>
2025-12-11 15:00:24 -0800

    Update SpaceStation.py

--- SpaceStation.py
+++ SpaceStation.py
@@ -1,6 +1,6 @@
-# Name: 
-# Date:
-# Period:
+# Name: Cooper Irish
+# Date: 12/11/2025
+# Period: 4
 # Lab: SpaceStation.py
 # Description: Students use web services to gather and process live data from the internet.
 #
@@ -12,22 +12,44 @@
 import json  # to interpret JSON data from a web service
 import urllib.request  # to load data from a specific web service api
 import turtle  # for images
+import time # for time
 
 # TODO: Step 1 Who is in space?
-
-
+url = 'http://api.open-notify.org/astros.json'
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+print('There are' ,result['number'], 'People in Space')
+print('They are:')
+for i in result['people']:
+    print('\t',i['name'], 'on the', i['craft'])
+    
 # TODO: Step 2 Where is the ISS?
+url = 'http://api.open-notify.org/iss-now.json'
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
 
+location = result['iss_position']
+lat = location['latitude']
+lon = location['longitude']
+print("Latitude:", lat)
+print("Longitude:", lon)
 
-# TODO: Step 3 When will the ISS be overhead?  Note: Change from Houston to Roseville
+screen = turtle.Screen()
+screen.setup(1280,640)
+screen.setworldcoordinates(-180,-90,180,90)
+screen.bgpic('map.gif')
 
+screen.register_shape('iss.gif')
+iss = turtle.Turtle()
+iss.shape('iss.gif')
+iss.setheading(90)
 
-# TODO: Step 4 Rewrite the program in a format that uses functions
-
-
+iss.penup()
+iss.goto(float(lon),float(lat))
 # TODO: Step 5 How could you make the program more interactive?
 # Click for new overhead location?
 # Ask User for location for overhead location?
 # Make the program update automatically?
 
 # use screen.mainloop() to keep the window open
+screen.mainloop()
